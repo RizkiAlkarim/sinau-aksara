6e858018301986ffd52005016c2d12db9a3ca329
Rizki Alkarim M <rizkiam75@gmail.com>
2023-03-20 05:25:58 +0700

    Refactor home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom"
 import Header from "../components/Header"
 
 function Home() {
+  const homeItems = [
+    {label: "Learn", href:"/learn"},
+    {label: "Quiz", href:"/quiz"},
+    {label: "About", href:"/about"},
+  ]
   let button = 'bg-orange-200 text-red-600 rounded-md px-3 py-2 font-bold text-lg text-center'
 
   return (
@@ -10,8 +15,9 @@ function Home() {
   <nav className='flex flex-col justify-center items-center gap-2 py-2 h-full'>
       <h1 className='text-4xl font-bold text-center mb-5'>Sugeng <br/> Rawuh</h1>
       <ul className='grid grid-rows-2 gap-3'>
-        <Link className={button} to="/learn">Learn</Link>
-        <Link className={button} to="/quiz">Quiz</Link>
+        {homeItems.map(({label, href})=>
+          <Link key={label} className={button} to={href}>{label}</Link>
+        )}
       </ul>
     </nav>
     </div>
