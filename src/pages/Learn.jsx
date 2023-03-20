d9e12dca4128b7b0714d107bcfa7cc9c24c122e6
Rizki Alkarim M <rizkiam75@gmail.com>
2023-03-20 13:36:26 +0700

    Delete comment in Learn file

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -13,19 +13,6 @@ function Learn() {
     <div className="h-screen flex flex-col">
       <Header />
       <Table />
-      {/* <nav className='flex flex-col justify-center items-center gap-2 py-2 h-full'>
-        <h1 className='sans text-4xl font-bold text-center mb-5'>Learn</h1>
-        <ul className='grid grid-rows-2 gap-3'>
-          { learnItems.map(({label, href}) =>
-          <Link
-            key={label}
-            className='bg-orange-200 text-red-600 rounded-md px-3 py-2 font-bold text-lg text-center'
-            to={href}>
-            {label}
-            </Link>
-          )}
-        </ul>
-      </nav> */}
     </div>
   )
 }
