580adf2ec1ec803c4a2607c59a82e27e9abfa89e
Rizki Alkarim M <rizkiam75@gmail.com>
2023-03-20 09:23:18 +0700

    Create aksara pasangan data file

---
+++ src/data/pasangan.js
@@ -0,0 +1,24 @@
+const pasangan = [
+    {aksara:'꧀ꦲ', letter:'h'},
+    {aksara:'꧀ꦤ', letter:'n'},
+    {aksara:'꧀ꦕ', letter:'c'},
+    {aksara:'꧀ꦫ', letter:'r'},
+    {aksara:'꧀ꦏ', letter:'k'},
+    {aksara:'꧀ꦢ', letter:'d'},
+    {aksara:'꧀ꦠ', letter:'t'},
+    {aksara:'꧀ꦱ', letter:'s'},
+    {aksara:'꧀ꦮ', letter:'w'},
+    {aksara:'꧀ꦭ', letter:'l'},
+    {aksara:'꧀ꦥ', letter:'p'},
+    {aksara:'꧀ꦝ', letter:'dh'},
+    {aksara:'꧀ꦗ', letter:'j'},
+    {aksara:'꧀ꦪ', letter:'y'},
+    {aksara:'꧀ꦚ', letter:'ny'},
+    {aksara:'꧀ꦩ', letter:'m'},
+    {aksara:'꧀ꦒ', letter:'g'},
+    {aksara:'꧀ꦧ', letter:'b'},
+    {aksara:'꧀ꦛ', letter:'th'},
+    {aksara:'꧀ꦔ', letter:'ng'}
+]
+
+export default pasangan;
