35a416bc149840f993b9582ca720881604c0095b
Rizki Alkarim M <rizkiam75@gmail.com>
2023-03-20 18:40:33 +0700

    Delete unused variable in Learn component

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -3,12 +3,6 @@ import Header from "../components/Header"
 import Table from "../components/Table"
 
 function Learn() {
-  const learnItems = [
-    {label: "Hanacaraka", href:"/hanacaraka"},
-    {label: "Sandhangan", href:"/sandhangan"},
-    {label: "Pasangan", href:"/pasangan"}
-  ]
-  
   return (
     <div className="h-screen flex flex-col">
       <Header />
