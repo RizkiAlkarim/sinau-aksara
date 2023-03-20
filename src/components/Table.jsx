bafa4f97a6a83250e53094198029f11554f33b75
Rizki Alkarim M <rizkiam75@gmail.com>
2023-03-20 11:41:21 +0700

    Make aksara pasangan table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,24 +5,31 @@ import next from '../assets/right-arrow.png'
 import hanacaraka from '../data/hanacaraka'
 import pasangan from '../data/pasangan'
 
-function Table() {
-    const [table, setTable] = useState(0)
+const tableItems = [
+    hanacaraka,
+    pasangan
+]
 
-    const tableItems = [
-        hanacaraka,
-        pasangan
-    ]
+const tableItemsName = [
+    'Hanacaraka',
+    'Pasangan'
+]
+
+function Table() {
+    const [tableIndex, setTableIndex] = useState(0)
 
     const previousTable = ()=> {
-        if(table > 0){
-        setTable((prev)=> prev - 1)
+        if(tableIndex > 0){
+        setTableIndex((prev)=> prev - 1)
         }
     }
 
     const nextTable = ()=> {
-        setTable((prev)=> prev + 1)
+        if(tableIndex < tableItems.length - 1)
+        setTableIndex((prev)=> prev + 1)
     }
 
+    console.log(tableItems.length)
 
   return (
     <div className='flex flex-col justify-center items-center px-5 h-full'>
@@ -30,13 +37,13 @@ function Table() {
             <Link>
                 <img className='h-4' src={back} alt="back" onClick={previousTable}/>
             </Link>
-            <h1 className='font-bold text-2xl'>{table}</h1>
+            <h1 className='font-bold text-2xl'>{tableItemsName[tableIndex]}</h1>
             <Link>
                 <img className='h-4' src={next} alt="next" onClick={nextTable}/>
             </Link>
         </div>
         <div className='grid grid-cols-5 grid-rows-4 place-items-center w-full bg-orange-200 rounded p-2'>
-            {hanacaraka.map(({aksara, letter})=>(
+            {tableItems[tableIndex].map(({aksara, letter})=>(
                 <div key={letter}>
                     <div className='font-bold text-3xl text-red-600 text-center'>{aksara}</div>
                     <div className='font-thin text-sm text-gray-700 text-center'>{letter}</div>
